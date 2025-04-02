790c1f7d6002091121219b806d07171ae0abc588
GG-Bruse <BJY811@outlook.com>
2025-04-03 00:15:57 +0800

    Tensor create

--- PyTorch/张量的创建.py
+++ PyTorch/张量的创建.py
@@ -53,3 +53,66 @@ print(data) # [start,end, numbers]
 
 
 print("\n创建随机张量")
+# 创建两行三列随机张量
+data = torch.randn(2, 3)
+print(data)
+print("随机数种子:", torch.initial_seed())
+# 设置随机数种子
+torch.manual_seed(100)
+data = torch.randn(2, 3)
+print(data)
+print("随机数种子:", torch.initial_seed())
+
+
+
+print("创建0、1、指定值的张量")
+# 创建指定形状的全0张量
+data = torch.zeros(2, 3)
+print(data)
+# 根据其他张量的形状创建全0张量
+data = torch.zeros_like(data)
+print(data)
+# 创建指定形状的全1张量
+data = torch.ones(2, 3)
+print(data)
+# 根据其他张量的形状创建全1张量
+data = torch.ones_like(data)
+print(data)
+# 创建指定形状的指定值张量
+data = torch.full([2, 3], 10)
+print(data)
+# 根据其他张量的形状创建指定值的张量
+data = torch.full_like(data, 20)
+print(data)
+
+
+
+print("\n张量元素类型转换")
+data = torch.full([2, 3], 10)
+print(data.dtype)
+# 1. 转换为其他类型
+data = data.type(torch.DoubleTensor)
+print(data.dtype)
+data = data.type(torch.ShortTensor)
+print(data.dtype)
+data = data.type(torch.IntTensor)
+print(data.dtype)
+data = data.type(torch.LongTensor)
+print(data.dtype)
+data = data.type(torch.FloatTensor)
+print(data.dtype)
+data = data.type(dtype=torch.float16)
+print(data.dtype)
+# 2. 转换为其他类型
+data = data.double()
+print(data.dtype)
+data = data.half()
+print(data.dtype)
+data = data.short()
+print(data.dtype)
+data = data.int()
+print(data.dtype)
+data = data.long()
+print(data.dtype)
+data = data.float()
+print(data.dtype)
